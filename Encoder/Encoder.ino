468abac239650dd7bc9595c39b685bcf0a704b39
Khaled Gabr <30967695+khaledgabr77@users.noreply.github.com>
2021-08-29 12:57:04 +0400

    Update Encoder.ino

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,26 +1,40 @@
+ 
 /*
- * Khaled Gabr
- * pwm 4 , 3 , 2 ,1
------ 10 , 9 , 8 , 6
-
-DIR 4 , 3 , 2 ,1
------5 , 4 , 3 , 2
-
-driver 4
-EA 22
-EB 24
-
-driver 3
-EA 26 clk
-EB 28 DT
-
-driver 2
-EA 30
-EB 32
-
-driver 1
-EA 34
-EB 36
+ * Copyright (c) 2021, Micropolis
+ * All rights reserved.
+ 
+ * Created on Sun Aug 29 12:56:17 2021
+ * @author: Khaled Gabr
+ 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/*
+ *  Encoder ROSSERIAL Node PUBLISHER test app
+ *
+ *  Copyright 2021  RoboPeak Team
+ *  http://www.micropolis.ae
+ * 
  */
  #include <ros.h>
  #include <std_msgs/Float32.h>
